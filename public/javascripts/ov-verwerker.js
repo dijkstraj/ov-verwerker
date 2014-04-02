22578fe7c77185dfd0e72b1d740e430959eea7ec
Jogchem Dijkstra <jogchem@gmail.com>
2014-04-02 21:42:26 +0200

    No more endless loops

--- public/javascripts/ov-verwerker.js
+++ public/javascripts/ov-verwerker.js
@@ -1,18 +1,30 @@
-var weekdays = ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'];
+function getCurrentPage() {
+  return parseInt($(".paginering li").has("em").text().trim());
+}
 
-do {
-  $("tr").has("td:contains('Check-uit')").css("background-color", "red").each(function() {
-    var dateStr = $(this).find("td").first().text().trim();
-    dateStr = dateStr.substr(6, 4) + "-" + dateStr.substr(3, 2) + "-" + dateStr.substr(0, 2);
-    var date = new Date(Date.parse(dateStr));
-    var dayOfWeek = date.getDay();
-    var workingDay = dayOfWeek >= 2 && dayOfWeek <= 4;
-    $(this).append("<td>" + weekdays[dayOfWeek] + "</td>");
-    $(this).find(":checkbox").each(function() {
-      if (workingDay != $(this).prop("checked")) {
-        $(this).click();
-      }
+var weekdays = ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'];
+var pageToCheck = getCurrentPage();
+
+function checkThisPage() {
+  if (getCurrentPage() == pageToCheck) {
+    $("tr").has("td:contains('Check-uit')").css("background-color", "red").each(function() {
+      var dateStr = $(this).find("td").first().text().trim();
+      dateStr = dateStr.substr(6, 4) + "-" + dateStr.substr(3, 2) + "-" + dateStr.substr(0, 2);
+      var date = new Date(Date.parse(dateStr));
+      var dayOfWeek = date.getDay();
+      var workingDay = dayOfWeek >= 2 && dayOfWeek <= 4;
+      $(this).append("<td>" + weekdays[dayOfWeek] + "</td>");
+      $(this).find(":checkbox").each(function() {
+        if (workingDay != $(this).prop("checked")) {
+          $(this).click();
+        }
+      });
     });
-  });
-  $(".volgende a").click();
-} while ($(".volgende a").size() > 0);
+    
+    pageToCheck++;
+    $(".volgende a").click();
+    setTimeout(checkThisPage, 1000);
+  }
+}
+
+checkThisPage();
